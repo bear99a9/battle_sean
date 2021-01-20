6be3825a9ef109f959d81b3cb3901ef63d917304
Sean Edwards <seantrevoredwards@gmail.com>
2021-01-20 19:39:17 +0000

    added helper file to dry up test code

--- spec/feature/enter_names_spec.rb
+++ spec/feature/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Players names' do
   scenario 'getting players names' do
-    visit('/')
-    fill_in :player_1_name, with: 'player_1_name'
-    fill_in :player_2_name, with: 'player_2_name'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'player_1_name vs. player_2_name'
   end
 end
--- spec/feature/hit_points_spec.rb
+++ spec/feature/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'Shows hit point' do
   scenario 'show player 2 hit points' do
-    visit('/')
-    fill_in :player_1_name, with: 'player_1_name'
-    fill_in :player_2_name, with: 'player_2_name'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'player_2_name: 100HP'
   end
 end
